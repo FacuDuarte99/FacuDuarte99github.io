667fc4dbebb0626fdd8072a85f9da00962d5a206
FacuDuarte99 <124009357+FacuDuarte99@users.noreply.github.com>
2023-01-30 22:14:38 -0300

    Add files via upload

---
+++ INDEX.js
@@ -0,0 +1,9 @@
+function Funcion(){
+    window.open('https://uba.ar/#/');
+}
+function FunFace(){
+    window.open('https://es-la.facebook.com/');
+}
+function FunCor(){
+    window.open('https://www.google.com/intl/es-419/gmail/about/');
+}
